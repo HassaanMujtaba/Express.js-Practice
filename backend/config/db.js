0919558bf95161231dacc09da2ad352e46a2d008
HassaanMujtaba <hassaanmujtaba1@gmail.com>
2023-07-27 03:34:02 -0700

    all done

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(
-      `mongodb+srv://hassaanmujtaba1:${process.env.MONGODB_PASSWORD}@express-cluster.lakv5oh.mongodb.net/`,
+      `mongodb+srv://hassaanmujtaba1:helloworld@cluster0.myrym5h.mongodb.net/?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       }
     );
-    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
     process.exit(1);
